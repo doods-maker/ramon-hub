63e2b4c2fe93a564c76bad25a1d4f549189e2fe5
Eduardo Schlata <adveduardoschlata@gmail.com>
2026-06-30 20:28:02 -0300

    feat: LeadStage garante no máximo uma etapa de ganho e uma de perda

--- app/models/lead_stage.rb
+++ app/models/lead_stage.rb
@@ -4,4 +4,17 @@ class LeadStage < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { scope: :account_id }
   default_scope { order(:position) }
+
+  before_save :ensure_single_won_lost
+
+  private
+
+  def ensure_single_won_lost
+    if is_won? && (new_record? || will_save_change_to_is_won?)
+      account.lead_stages.where.not(id: id).update_all(is_won: false)
+    end
+    if is_lost? && (new_record? || will_save_change_to_is_lost?)
+      account.lead_stages.where.not(id: id).update_all(is_lost: false)
+    end
+  end
 end
