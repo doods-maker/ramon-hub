d3a45e30d1fe416849079556af526f6307602782
Eduardo Schlata <adveduardoschlata@gmail.com>
2026-06-30 20:30:47 -0300

    feat: rotas CRUD/reorder + políticas admin-only do config do funil

---
+++ app/policies/benefit_type_policy.rb
@@ -0,0 +1,6 @@
+class BenefitTypePolicy < ApplicationPolicy
+  def create? = @account_user.administrator?
+  def update? = @account_user.administrator?
+  def destroy? = @account_user.administrator?
+  def reorder? = @account_user.administrator?
+end
