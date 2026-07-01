46d77c23ab8e2dad540744a76bb992a826190000
Eduardo Schlata <adveduardoschlata@gmail.com>
2026-07-01 16:46:58 -0300

    feat: stop navigating to inbox on open-conversation (dock handles it)

--- app/javascript/dashboard/routes/dashboard/ramon/pages/Funil.vue
+++ app/javascript/dashboard/routes/dashboard/ramon/pages/Funil.vue
@@ -1,30 +1,14 @@
 <script setup>
 import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
 import KanbanBoard from '../components/kanban/KanbanBoard.vue';
 import NewLeadModal from '../components/kanban/NewLeadModal.vue';
 
-const route = useRoute();
-const router = useRouter();
 const showModal = ref(false);
-
-const openConversation = conversationId => {
-  router.push({
-    name: 'inbox_conversation',
-    params: {
-      accountId: route.params.accountId,
-      conversation_id: conversationId,
-    },
-  });
-};
 </script>
 
 <template>
   <div class="flex flex-col w-full h-full bg-n-background">
-    <KanbanBoard
-      @new-lead="showModal = true"
-      @open-conversation="openConversation"
-    />
+    <KanbanBoard @new-lead="showModal = true" />
     <NewLeadModal v-if="showModal" @close="showModal = false" />
   </div>
 </template>
