b8b0c82433c06f5a3ebdfea8dcc84f9c3034811e
Eduardo Schlata <adveduardoschlata@gmail.com>
2026-07-07 11:07:36 -0300

    test(ntfy): assert request body includes lead benefit in success case

--- spec/jobs/ramon/ntfy_push_job_spec.rb
+++ spec/jobs/ramon/ntfy_push_job_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Ramon::NtfyPushJob do
   let(:account) { create(:account) }
-  let(:lead) { create(:lead, account: account, name: 'João da Silva') }
+  let(:benefit_type) { create(:benefit_type, account: account, name: 'Auxílio-Acidente') }
+  let(:lead) { create(:lead, account: account, name: 'João da Silva', benefit_type: benefit_type) }
 
   describe '#perform' do
     context 'quando NTFY_TOPIC está setado' do
       it 'faz POST pro tópico com título transliterado e corpo com o benefício' do
         with_modified_env NTFY_TOPIC: 'ramon-leads' do
           stub = stub_request(:post, 'https://ntfy.sh/ramon-leads')
-                 .with(headers: { 'Title' => 'Novo lead: Joao da Silva', 'Tags' => 'bell' })
+                 .with(
+                   headers: { 'Title' => 'Novo lead: Joao da Silva', 'Tags' => 'bell' },
+                   body: /Auxílio-Acidente/
+                 )
                  .to_return(status: 200)
 
           described_class.perform_now(lead.id)
